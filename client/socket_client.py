52040dae69681999a8f7a152c1059b715d9821d7
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-06-09 21:02:55 +0200

    Add comments

--- client/socket_client.py
+++ client/socket_client.py
@@ -7,50 +7,82 @@ import time
 BUFFER_SIZE = 4096
 SERVER_ADDRESS = ('localhost', 49000)
 
+
 def get(fileName):
+    
+    # Send command to server
     socket.sendto((command + ' "' + fileName + '"').encode(), SERVER_ADDRESS)
     received, server = socket.recvfrom(BUFFER_SIZE)
+    
+    # Receive file size from the server
     if (int(received.decode('utf8')) == 1):
         origin_file_size, server = socket.recvfrom(BUFFER_SIZE)
         origin_file_size = int(origin_file_size.decode('utf8'))
+        
         print ('\nDownloading' + ' ' + fileName + ' ' + str(origin_file_size) + ' bytes')
+        
+        # Receive bytes of the file
         with open(fileName, 'wb') as file:
             while True:
                 bytes_read = socket.recv(BUFFER_SIZE)
+                
+                # File transmitting done
                 if not bytes_read:
                     break
+                
+                # Write bytes on file
                 file.write(bytes_read)  
-        file_size = os.path.getsize(fileName) #bytes ricevuti
+                
+        # Bytes received
+        file_size = os.path.getsize(fileName)
+        
+        # Check for error
         if file_size != origin_file_size:
             print('\nERROR\n')
             print(str(origin_file_size - file_size) + ' bytes lost\n')
         else:
             print("\nFile downloaded correctly")
+            
     else:
         print('\nERROR\n')
         print("The file is not in the server")
         
+        
 def put(fileName):
+    
     with open(fileName, 'rb') as file:
-        file_size = os.path.getsize(fileName)#bytes inviati
+
+        # Bytes to send
+        file_size = os.path.getsize(fileName)
         print ('\nUploading' + ' ' + fileName + ' ' + str(file_size) + ' bytes')
         socket.sendto((command + ' "' + fileName + '"').encode(), SERVER_ADDRESS)
+        
+        # Send bytes
         while True:
             time.sleep(0.0001)
             bytes_read = file.read(BUFFER_SIZE)
+            
+            # File transmitting done
             if not bytes_read:
                 socket.sendto(''.encode(), SERVER_ADDRESS)
-                break # file transmitting done
+                break
+                
             socket.sendto(bytes_read, SERVER_ADDRESS)
+            
+    # Receive file size from the server
     received_file_size, server = socket.recvfrom(BUFFER_SIZE)
     received_file_size = int(received_file_size.decode('utf8'))
+    
+    # Check for error
     if file_size != received_file_size:
         print('\nERROR\n')
         print(str(file_size - received_file_size) + ' bytes lost\n')
     else:
         print("\nFile uploaded correctly")
 
+
 while True:
+    
     # Socket creation
     socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
         
@@ -62,23 +94,26 @@ while True:
     try:
         if len(shlex.split(command)) == 2:
             command , fileName = shlex.split(command);
+            
             if command == 'put':
                 put(fileName)
+            elif command == 'get':
+                get(fileName)
             else:
-                if command == 'get':
-                    get(fileName)
-                else:
-                    data = "Error".encode()
-                    print("The command is not supported")
+                data = "Error".encode()
+                print("The command is not supported")
         else:
+            #Send command to the server
             socket.sendto(command.encode(), SERVER_ADDRESS)
             data, server = socket.recvfrom(BUFFER_SIZE)
+            
+            #Print server response
             print ('\n%s\n' % data.decode('utf8'))
         
         
     except Exception as info:
         print(info)
+        
     finally:
         print ('\nClosing socket')
         socket.close()
-
--- server/socket_server.py
+++ server/socket_server.py
@@ -9,40 +9,67 @@ from os.path import isfile, join
 BUFFER_SIZE = 4096
 ADDRESS = ('localhost', 49000)
 
+
 def get(fileName, filesList):
+    
     print("Received command get")
+    
     if fileName in filesList:
-        file_size = os.path.getsize('./serverFiles/' + fileName)#bytes inviati
+        # Bytes to send
+        file_size = os.path.getsize('./serverFiles/' + fileName)
+        
+        # Send file size
         with open('./serverFiles/' + fileName, 'rb') as file:
             sock.sendto('1'.encode(), ADDRESS)
             sock.sendto((str(file_size)).encode(), ADDRESS)
+
+            # Send bytes
             while True:
                 time.sleep(0.0001)
                 bytes_read = file.read(BUFFER_SIZE)
+                
+                # File transmitting done
                 if not bytes_read:
                     sock.sendto(''.encode(), ADDRESS)
-                    break # file transmitting done
+                    break
+                    
                 sock.sendto(bytes_read, ADDRESS)
+                
         print("File downloaded")
+        
     else:
-        #ERROR
-        #When the file does not exist
+        # ERROR
+        # When the file does not exist
         print("Error")
         sock.sendto('0'.encode(), ADDRESS)
         
+        
 def put(fileName, filesList):
+    
     print("Received command put")
+    
+    # Add file to files list
     fileName = os.path.basename(fileName)
     filesList.append(fileName)
+    
+    # Receive bytes of the file
     with open('./serverFiles/' + fileName, 'wb') as file:
         while True:
             bytes_read = sock.recv(BUFFER_SIZE)
+            
+            # File transmitting done
             if not bytes_read:
                 break
+            
+            # Write bytes on file
             file.write(bytes_read)
-    file_size = os.path.getsize('./serverFiles/' + fileName) #bytes ricevuti
+    
+    # Bytes received
+    file_size = os.path.getsize('./serverFiles/' + fileName)
     sock.sendto(str(file_size).encode(), ADDRESS)
+    
     print("File uploaded")
+    
 
 # Socket creation
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
@@ -51,30 +78,35 @@ sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 sock.bind(ADDRESS)
 print ('\n\rstarting up on %s port %s' % ADDRESS)
 
+# List that contains the files
 filesList = [f for f in listdir("./serverFiles") if isfile(join("./serverFiles", f))]
 
 while True:
     
     print('\n\rWaiting to receive message...')
+    
+    # Update files list
     data, ADDRESS = sock.recvfrom(BUFFER_SIZE)
     filesList = [f for f in listdir("./serverFiles") if isfile(join("./serverFiles", f))]
     data = data.decode('utf8')
+    
     if data == 'list':
         if len(filesList) > 0:
             sock.sendto('\n'.join(filesList).encode(), ADDRESS)
             print("Files list sent")
         else:
-            #There are no files in the server files
+            # There are no files in the server files
             print("Error")
             sock.sendto('Files list is empty.'.encode(), ADDRESS)
+            
     else:
         if len(shlex.split(data)) < 2:
-            #ERROR
-            #When the command is not supported
+            # ERROR
+            # When the command is not supported
             sock.sendto('Command not recognized'.encode(), ADDRESS)
         elif len(shlex.split(data)) > 2:
-            #ERROR
-            #When the command is not complete
+            # ERROR
+            # When the command is not complete
             print("Error")
             sock.sendto('With get and put commands is required one argument: FILE_NAME'.encode(), ADDRESS)
         else:
@@ -84,8 +116,8 @@ while True:
             elif command == 'put':
                 put(fileName, filesList)
             else:
-                #ERROR
-                #When the command is not supported
+                # ERROR
+                # When the command is not supported
                 print("Error")
                 sock.sendto('Command not recognized'.encode(), ADDRESS)
                 
